3339bad6e9899f86e5ef7e60aa91f450d6d371ab
Sarath Vadlamannati <venkatasaisarathsarma.vadlamannatilakshmi@hitachivantara.com>
2023-03-14 13:32:51 +0530

    windows reading out text fn added

--- harvis.py
+++ harvis.py
@@ -1,16 +1,22 @@
 import gradio as gr
 import openai, config, subprocess
 openai.api_key = config.OPENAI_API_KEY
+import os
 
 
 
 messages = [{"role": "system", "content": 'ENTER_YOUR_CONTEXT_HERE'}]
 
+# therapist
+# messages = [{"role": "system", "content": 'You are a therapist.'}]
 # examples of few content  - content is basically the context you are setting for your conversation with Harvis :D 
 # Ex1: You are a Storage Solutions Expert. Respond to all input in 100 words or less.
 # Ex2: You are a financial Advisor. Response in more than 500 words.
-# Ex3: You are a nutritionist expert.
+# Ex3: You are a nutritionist expert but you will always answer my questions in the form of a rap song.
 
+def powershell_run(cmd):
+    completed = subprocess.run(["powershell", "-Command", cmd], capture_output=True)
+    return completed
 
 def transcribe(audio):
     try:
@@ -26,7 +32,10 @@ def transcribe(audio):
         system_message = response["choices"][0]["message"]
         messages.append(system_message)
 
-        subprocess.call(["say", system_message['content']], shell=True)
+        if os.name == 'nt':
+            powershell_run("Add-Type -AssemblyName System.Speech; (New-Object System.Speech.Synthesis.SpeechSynthesizer).Speak('{}');".format(system_message['content']))
+        else:
+            subprocess.call(["say", system_message['content']])
 
         chat_transcript = ""
         for message in messages:
